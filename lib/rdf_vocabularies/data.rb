f243a298acec4199f06cc9619ba44ea11cd8e306
Collin Brittle <cnbrittle@gmail.com>
2014-05-09 16:27:44 -0400

    Add rdf vocab in lib.

---
+++ lib/rdf_vocabularies/data.rb
@@ -0,0 +1,10 @@
+class DataVocabulary < RDF::Vocabulary("http://www.lib.vt.edu/data_vocab#")
+  property :last_job
+  property :mean
+  property :hi
+  property :lo
+  property :data_columns
+  property :samples
+  property :sample_freq
+  property :setup_params
+end
